933a99bb0c39e8017f79cdade29d2d69bb9de15d
Wuzzy <almikes@aol.com>
2017-06-16 05:32:17 +0200

    Make sheep eat grass as well

--- sheep.lua
+++ sheep.lua
@@ -77,11 +77,19 @@ mobs:register_mob("mobs_mc:sheep", {
 	},
 	follow = "farming:wheat",
 	view_range = 12,
-	
+
+	-- Eat grass
 	replace_rate = 20,
-	replace_what = "default:dirt_with_grass",
-	replace_with = "default:dirt",
-	replace_offset = -1,
+	replace_what = {
+		-- “Grass Block”
+		{ "default:dirt_with_grass", "default:dirt", -1 },
+		-- “Tall Grass”
+		{ "default:grass_5", "air", 0 },
+		{ "default:grass_4", "air", 0 },
+		{ "default:grass_3", "air", 0 },
+		{ "default:grass_2", "air", 0 },
+		{ "default:grass_1", "air", 0 },
+	},
 	
 	-- Set random color on spawn
 	do_custom = function(self)
