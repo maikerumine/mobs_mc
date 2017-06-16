81acb8188b6c98f0305a01c2a703485abc96c6b1
Wuzzy <almikes@aol.com>
2017-06-16 17:19:48 +0200

    Add rabbit items

--- rabbit.lua
+++ rabbit.lua
@@ -29,7 +29,8 @@ local rabbit = {
 	runaway = true,
 	jump = true,
 	drops = {
-		{name = "mobs:meat_raw", chance = 1, min = 1, max = 1},
+		{name = "mobs_mc:rabbit_raw", chance = 1, min = 0, max = 1},
+		{name = "mobs_mc:rabbit_hide", chance = 1, min = 0, max = 1},
 	},
 	water_damage = 1,
 	lava_damage = 4,
@@ -184,6 +185,39 @@ mobs:register_egg("mobs_mc:rabbit", "Rabbit", "rabbit_inv.png", 0)
 
 -- Note: This spawn egg does not exist in Minecraft
 mobs:register_egg("mobs_mc:killer_bunny", "Killer Bunny", "rabbit_inv.png^[colorize:#FF0000:192", 0) -- TODO: Update inventory image
+
+-- Items
+minetest.register_craftitem("mobs_mc:rabbit_raw", {
+	description = "Raw Rabbit",
+	inventory_image = "mcl_mobitems_rabbit_raw.png",
+	on_use = minetest.item_eat(3),
+})
+
+minetest.register_craftitem("mobs_mc:rabbit_cooked", {
+	description = "Cooked Rabbit",
+	inventory_image = "mcl_mobitems_rabbit_cooked.png",
+	on_use = minetest.item_eat(5),
+})
+
+minetest.register_craft({
+	type = "cooking",
+	output = "mobs_mc:rabbit_cooked",
+	recipe = "mobs_mc:rabbit_raw",
+	cooktime = 5,
+})
+
+minetest.register_craftitem("mobs_mc:rabbit_hide", {
+	description = "Rabbit Hide",
+	inventory_image = "mcl_mobitems_rabbit_hide.png"
+})
+
+minetest.register_craft({
+	output = "mobs:leather",
+	recipe = {
+		{ "mobs_mc:rabbit_hide", "mobs_mc:rabbit_hide" },
+		{ "mobs_mc:rabbit_hide", "mobs_mc:rabbit_hide" },
+	}
+})
 
 -- compatibility
 mobs:alias_mob("mobs:bunny", "mobs_mc:rabbit")
