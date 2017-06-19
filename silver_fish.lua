1bb691349ce99ca4317059c5537e7d1a2a5ceb96
Wuzzy <almikes@aol.com>
2017-06-19 16:03:54 +0200

    Remove unused silverfish code

--- silver_fish.lua
+++ silver_fish.lua
@@ -1,38 +1,6 @@
-
 --###################
 --################### SILVERFISH
 --###################
---[[
-mobs:register_mob("mobs_mc:25silverfish", {
-	type = "animal",
-	passive = true,
-    runaway = true,
-    stepheight = 1.2,
-	hp_min = 30,
-	hp_max = 60,
-	armor = 150,
-    collisionbox = {-0.35, -0.01, -0.35, 0.35, 2, 0.35},
-    rotate = -180,
-	visual = "mesh",
-	mesh = "silverfish.b3d",
-	textures = {
-		{"silverfish.png"},
-	},
-	visual_size = {x=3, y=3},
-	walk_velocity = 0.6,
-	run_velocity = 2,
-	jump = true,
-	animation = {
-		speed_normal = 25,		speed_run = 50,
-		stand_start = 0,		stand_end = 20,
-		walk_start = 0,		walk_end = 20,
-		run_start = 0,		run_end = 20,
-	},
-})
-
-mobs:register_egg("mobs_mc:25silverfish", "Silverfish", "silverfish_inv.png", 0)
-]]
--- Bunny by ExeterDad
 
 mobs:register_mob("mobs_mc:silverfish", {
 	type = "monster",
@@ -73,6 +41,5 @@ mobs:register_mob("mobs_mc:silverfish", {
 mobs:register_spawn("mobs_mc:silverfish",
 	{"default:diorite"}, 20, 10, 15000, 2, 80, true)
 
---mobs:register_egg("mobs_mc:bunny", S("Bunny"), "mobs_bunny_inv.png", 0)
 mobs:register_egg("mobs_mc:silverfish", "Silverfish", "silverfish_inv.png", 0)
 
