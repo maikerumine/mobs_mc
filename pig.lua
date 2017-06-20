18bf29c0d7969afb72d194610871ec52391c86b2
Wuzzy <almikes@aol.com>
2017-06-20 18:44:11 +0200

    Pig: Make carrotstick usable, fix feeding

--- pig.lua
+++ pig.lua
@@ -48,7 +48,7 @@ mobs:register_mob("mobs_mc:pig", {
 		look_start = 78,
 		look_end = 108,
 	},
-	follow = {"default:apple", "farming:potato", "farming:carrot"},
+	follow = {"default:apple", "farming:potato", "farming:carrot", "mobs_mc:carrot_on_a_stick"},
 	view_range = 5,
 	do_custom = function(self, dtime)
 
@@ -88,51 +88,76 @@ mobs:register_mob("mobs_mc:pig", {
 	end,
 
 	on_rightclick = function(self, clicker)
-
-		-- make sure player is clicking
 		if not clicker or not clicker:is_player() then
 			return
 		end
 
-		-- feed, tame or heal horse
-		if mobs:feed_tame(self, clicker, 10, true, true) then
-			return
-		end
-
-		-- make sure tamed horse is being clicked by owner only
-		if self.tamed and self.owner == clicker:get_player_name() then
-
-			local inv = clicker:get_inventory()
-
-			-- detatch player already riding horse
-			if self.driver and clicker == self.driver then
-
-				mobs.detach(clicker, {x = 1, y = 0, z = 1})
-
-				-- add saddle back to inventory
-				if inv:room_for_item("main", "mobs:saddle") then
-					inv:add_item("main", "mobs:saddle")
-				else
-					minetest.add_item(clicker.getpos(), "mobs:saddle")
-				end
-
-			-- attach player to horse
-			elseif not self.driver
-			and clicker:get_wielded_item():get_name() == "mobs:saddle" then
-
-				self.object:set_properties({stepheight = 1.1})
-				mobs.attach(self, clicker)
-
-				-- take saddle from inventory
-				inv:remove_item("main", "mobs:saddle")
+		local wielditem = clicker:get_wielded_item()
+		-- Feed pig
+		if wielditem:get_name() ~= "mobs_mc:carrot_on_a_stick" then
+			if mobs:feed_tame(self, clicker, 1, true, true) then
+				return
 			end
 		end
 
-		-- used to capture horse with magic lasso
-		mobs:capture_mob(self, clicker, 0, 0, 80, false, nil)
-	end
+		-- Put saddle on pig
+		local item = clicker:get_wielded_item()
+		if item:get_name() == "mobs:saddle" and self.saddle ~= "yes" then
+			self.object:set_properties({
+				textures = {"mobs_pig_with_saddle.png"},
+			})
+			self.saddle = "yes"
+			self.tamed = true
+			self.drops = {
+				{name = "mobs:pork_raw",
+				chance = 1,
+				min = 1,
+				max = 3,},
+				{name = "mobs:saddle",
+				chance = 1,
+				min = 1,
+				max = 1,},
+			}
+			if not minetest.settings:get_bool("creative_mode") then
+				local inv = clicker:get_inventory()
+				local stack = inv:get_stack("main", clicker:get_wield_index())
+				stack:take_item()
+				inv:set_stack("main", clicker:get_wield_index(), stack)
+			end
+			return
+		end
 
+		-- Mount or detach player
+		local name = clicker:get_player_name()
+		if self.driver and clicker == self.driver then
+			-- Detach if already attached
+			mobs.detach(clicker, {x=1, y=0, z=0})
+			return
+
+		elseif not self.driver and self.saddle == "yes" and wielditem:get_name() == "mobs_mc:carrot_on_a_stick" then
+			-- Ride pig if it has a saddle and player uses a carrot on a stick
+
+			mobs.attach(self, clicker)
+
+			if not minetest.settings:get_bool("creative_mode") then
+
+				local inv = self.driver:get_inventory()
+				-- 26 uses
+				if wielditem:get_wear() > 63000 then
+					wielditem = {name = "fishing:pole_wood", count = 1}
+				else
+					wielditem:add_wear(2521)
+				end
+				inv:set_stack("main",self.driver:get_wield_index(), wielditem)
+			end
+			return
+		end
+
+		-- used to capture pig with magic lasso
+		mobs:capture_mob(self, clicker, 0, 0, 80, false, nil)
+	end,
 })
+
 mobs:register_spawn("mobs_mc:pig", {"default:dirt_with_grass"}, minetest.LIGHT_MAX, 9, 15000, 30, 31000)
 	
 
@@ -209,6 +234,6 @@ mobs:alias_mob("mobs:pig", "mobs_mc:pig")
 -- spawn eggs
 mobs:register_egg("mobs_mc:pig", "Pig", "pig_inv.png", 0)
 
-if minetest.setting_get("log_mods") then
+if minetest.settings:get("log_mods") then
 	minetest.log("action", "MC Pig loaded")
 end
