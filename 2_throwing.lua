00f1816bdfd40d6f17849a5783a3b2ca588222b5
Wuzzy <almikes@aol.com>
2017-06-23 16:29:09 +0200

    Make all items overwritable

--- 2_throwing.lua
+++ 2_throwing.lua
@@ -6,73 +6,92 @@
 
 --maikerumines throwing code
 --arrow (weapon)
-minetest.register_craftitem("mobs_mc:arrow", {
-	description = "Arrow",
-	inventory_image = "throwing_arrow_2.png",
-})
 
-minetest.register_node("mobs_mc:arrow_box", {
-	drawtype = "nodebox",
-	node_box = {
-		type = "fixed",
-		fixed = {
-			-- Shaft
-			{-6.5/17, -1.5/17, -1.5/17, 6.5/17, 1.5/17, 1.5/17},
-			--Spitze
-			{-4.5/17, 2.5/17, 2.5/17, -3.5/17, -2.5/17, -2.5/17},
-			{-8.5/17, 0.5/17, 0.5/17, -6.5/17, -0.5/17, -0.5/17},
-			--Federn
-			{6.5/17, 1.5/17, 1.5/17, 7.5/17, 2.5/17, 2.5/17},
-			{7.5/17, -2.5/17, 2.5/17, 6.5/17, -1.5/17, 1.5/17},
-			{7.5/17, 2.5/17, -2.5/17, 6.5/17, 1.5/17, -1.5/17},
-			{6.5/17, -1.5/17, -1.5/17, 7.5/17, -2.5/17, -2.5/17},
+local c
+if minetest.get_modpath("mobs_mc_gameconfig") and mobs_mc.override and mobs_mc.override.items then
+	c = function(id)
+		return mobs_mc.override.items[id] == nil
+	end
+else
+	-- No items are overwritten, so always return true
+	c = function(id)
+		return true
+	end
+end
+if c("arrow") then
+	minetest.register_craftitem("mobs_mc:arrow", {
+		description = "Arrow",
+		inventory_image = "throwing_arrow_2.png",
+	})
 
-			{7.5/17, 2.5/17, 2.5/17, 8.5/17, 3.5/17, 3.5/17},
-			{8.5/17, -3.5/17, 3.5/17, 7.5/17, -2.5/17, 2.5/17},
-			{8.5/17, 3.5/17, -3.5/17, 7.5/17, 2.5/17, -2.5/17},
-			{7.5/17, -2.5/17, -2.5/17, 8.5/17, -3.5/17, -3.5/17},
-		}
-	},
-	tiles = {"throwing_arrow.png", "throwing_arrow.png", "throwing_arrow_back.png", "throwing_arrow_front.png", "throwing_arrow_2.png", "throwing_arrow.png"},
-	groups = {not_in_creative_inventory=1},
-})
+	minetest.register_node("mobs_mc:arrow_box", {
+		drawtype = "nodebox",
+		node_box = {
+			type = "fixed",
+			fixed = {
+				-- Shaft
+				{-6.5/17, -1.5/17, -1.5/17, 6.5/17, 1.5/17, 1.5/17},
+				--Spitze
+				{-4.5/17, 2.5/17, 2.5/17, -3.5/17, -2.5/17, -2.5/17},
+				{-8.5/17, 0.5/17, 0.5/17, -6.5/17, -0.5/17, -0.5/17},
+				--Federn
+				{6.5/17, 1.5/17, 1.5/17, 7.5/17, 2.5/17, 2.5/17},
+				{7.5/17, -2.5/17, 2.5/17, 6.5/17, -1.5/17, 1.5/17},
+				{7.5/17, 2.5/17, -2.5/17, 6.5/17, 1.5/17, -1.5/17},
+				{6.5/17, -1.5/17, -1.5/17, 7.5/17, -2.5/17, -2.5/17},
 
+				{7.5/17, 2.5/17, 2.5/17, 8.5/17, 3.5/17, 3.5/17},
+				{8.5/17, -3.5/17, 3.5/17, 7.5/17, -2.5/17, 2.5/17},
+				{8.5/17, 3.5/17, -3.5/17, 7.5/17, 2.5/17, -2.5/17},
+				{7.5/17, -2.5/17, -2.5/17, 8.5/17, -3.5/17, -3.5/17},
+			}
+		},
+		tiles = {"throwing_arrow.png", "throwing_arrow.png", "throwing_arrow_back.png", "throwing_arrow_front.png", "throwing_arrow_2.png", "throwing_arrow.png"},
+		groups = {not_in_creative_inventory=1},
+	})
 
+	local THROWING_ARROW_ENTITY={
+		physical = false,
+		timer=0,
+		visual = "wielditem",
+		visual_size = {x=0.1, y=0.1},
+		textures = {"mobs_mc:arrow_box"},
+		velocity = 10,
+		lastpos={},
+		collisionbox = {0,0,0,0,0,0},
+	}
 
-local THROWING_ARROW_ENTITY={
-	physical = false,
-	timer=0,
-	visual = "wielditem",
-	visual_size = {x=0.1, y=0.1},
-	textures = {"mobs_mc:arrow_box"},
-	velocity = 10,
-	lastpos={},
-	collisionbox = {0,0,0,0,0,0},
-}
+	--ARROW CODE
+	THROWING_ARROW_ENTITY.on_step = function(self, dtime)
+		self.timer=self.timer+dtime
+		local pos = self.object:getpos()
+		local node = minetest.get_node(pos)
 
+		minetest.add_particle({
+		    pos = pos,
+		    vel = {x=0, y=0, z=0},
+		    acc = {x=0, y=0, z=0},
+		    expirationtime = .3,
+		    size = 1,
+		    collisiondetection = false,
+		    vertical = false,
+		    texture = "mobs_mc_arrow_particle.png",
+		})
 
---ARROW CODE
-THROWING_ARROW_ENTITY.on_step = function(self, dtime)
-	self.timer=self.timer+dtime
-	local pos = self.object:getpos()
-	local node = minetest.get_node(pos)
-
-minetest.add_particle({
-    pos = pos,
-    vel = {x=0, y=0, z=0},
-    acc = {x=0, y=0, z=0},
-    expirationtime = .3,
-    size = 1,
-    collisiondetection = false,
-    vertical = false,
-    texture = "mobs_mc_arrow_particle.png",
-})
-
-	if self.timer>0.2 then
-		local objs = minetest.get_objects_inside_radius({x=pos.x,y=pos.y,z=pos.z}, 1.5)
-		for k, obj in pairs(objs) do
-			if obj:get_luaentity() ~= nil then
-				if obj:get_luaentity().name ~= "mobs_mc:arrow_entity" and obj:get_luaentity().name ~= "__builtin:item" then
+		if self.timer>0.2 then
+			local objs = minetest.get_objects_inside_radius({x=pos.x,y=pos.y,z=pos.z}, 1.5)
+			for k, obj in pairs(objs) do
+				if obj:get_luaentity() ~= nil then
+					if obj:get_luaentity().name ~= "mobs_mc:arrow_entity" and obj:get_luaentity().name ~= "__builtin:item" then
+						local damage = 3
+						minetest.sound_play("damage", {pos = pos})
+						obj:punch(self.object, 1.0, {
+							full_punch_interval=1.0,
+							damage_groups={fleshy=damage},
+						}, nil)
+						self.object:remove()
+					end
+				else
 					local damage = 3
 					minetest.sound_play("damage", {pos = pos})
 					obj:punch(self.object, 1.0, {
@@ -81,207 +100,202 @@ minetest.add_particle({
 					}, nil)
 					self.object:remove()
 				end
-			else
-				local damage = 3
-				minetest.sound_play("damage", {pos = pos})
-				obj:punch(self.object, 1.0, {
-					full_punch_interval=1.0,
-					damage_groups={fleshy=damage},
-				}, nil)
+			end
+		end
+
+		if self.lastpos.x~=nil then
+			if node.name ~= "air" then
+				minetest.sound_play("bowhit1", {pos = pos})
+				minetest.add_item(self.lastpos, 'mobs_mc:arrow')
 				self.object:remove()
 			end
 		end
+		self.lastpos={x=pos.x, y=pos.y, z=pos.z}
 	end
 
-	if self.lastpos.x~=nil then
-		if node.name ~= "air" then
-			minetest.sound_play("bowhit1", {pos = pos})
-			minetest.add_item(self.lastpos, 'mobs_mc:arrow')
-			self.object:remove()
+
+	minetest.register_entity("mobs_mc:arrow_entity", THROWING_ARROW_ENTITY)
+
+
+	minetest.register_craft({
+		output = 'mobs_mc:arrow 4',
+		recipe = {
+			{mobs_mc.items.flint},
+			{mobs_mc.items.stick},
+			{mobs_mc.items.feather},
+		}
+	})
+
+
+	arrows = {
+		{"mobs_mc:arrow", "mobs_mc:arrow_entity" },
+	}
+
+	local throwing_shoot_arrow = function(itemstack, player)
+		for _,arrow in ipairs(arrows) do
+			if player:get_inventory():get_stack("main", player:get_wield_index()+1):get_name() == arrow[1] then
+				if not minetest.settings:get_bool("creative_mode") then
+					player:get_inventory():remove_item("main", arrow[1])
+				end
+				local playerpos = player:getpos()
+				--local obj = minetest.env:add_entity({x=playerpos.x,y=playerpos.y+1.5,z=playerpos.z}, arrow[2]) --current
+				local obj = minetest.add_entity({x=playerpos.x,y=playerpos.y+1.5,z=playerpos.z}, arrow[2])  --mc
+				local dir = player:get_look_dir()
+				obj:setvelocity({x=dir.x*22, y=dir.y*22, z=dir.z*22})
+				obj:setacceleration({x=dir.x*-3, y=-10, z=dir.z*-3})
+				obj:setyaw(player:get_look_yaw()+math.pi)
+				minetest.sound_play("throwing_sound", {pos=playerpos})
+				if obj:get_luaentity().player == "" then
+					obj:get_luaentity().player = player
+				end
+				obj:get_luaentity().node = player:get_inventory():get_stack("main", 1):get_name()
+				return true
+			end
 		end
+		return false
 	end
-	self.lastpos={x=pos.x, y=pos.y, z=pos.z}
 end
 
-
-minetest.register_entity("mobs_mc:arrow_entity", THROWING_ARROW_ENTITY)
-
-
-minetest.register_craft({
-	output = 'mobs_mc:arrow 4',
-	recipe = {
-		{mobs_mc.items.flint},
-		{mobs_mc.items.stick},
-		{mobs_mc.items.feather},
-	}
-})
-
-
-arrows = {
-	{"mobs_mc:arrow", "mobs_mc:arrow_entity" },
-}
-
-local throwing_shoot_arrow = function(itemstack, player)
-	for _,arrow in ipairs(arrows) do
-		if player:get_inventory():get_stack("main", player:get_wield_index()+1):get_name() == arrow[1] then
-			if not minetest.settings:get_bool("creative_mode") then
-				player:get_inventory():remove_item("main", arrow[1])
+if c("bow") then
+	minetest.register_tool("mobs_mc:bow_wood", {
+		description = "Bow",
+		inventory_image = "mobs_mc_bow.png",
+		on_use = function(itemstack, user, pointed_thing)
+			if throwing_shoot_arrow(itemstack, user, pointed_thing) then
+				if not minetest.settings:get_bool("creative_mode") then
+					itemstack:add_wear(65535/50)
+				end
 			end
-			local playerpos = player:getpos()
-			--local obj = minetest.env:add_entity({x=playerpos.x,y=playerpos.y+1.5,z=playerpos.z}, arrow[2]) --current
-			local obj = minetest.add_entity({x=playerpos.x,y=playerpos.y+1.5,z=playerpos.z}, arrow[2])  --mc
-			local dir = player:get_look_dir()
-			obj:setvelocity({x=dir.x*22, y=dir.y*22, z=dir.z*22})
-			obj:setacceleration({x=dir.x*-3, y=-10, z=dir.z*-3})
-			obj:setyaw(player:get_look_yaw()+math.pi)
-			minetest.sound_play("throwing_sound", {pos=playerpos})
-			if obj:get_luaentity().player == "" then
-				obj:get_luaentity().player = player
-			end
-			obj:get_luaentity().node = player:get_inventory():get_stack("main", 1):get_name()
-			return true
-		end
-	end
-	return false
+			return itemstack
+		end,
+	})
+
+	minetest.register_craft({
+		output = 'mobs_mc:bow_wood',
+		recipe = {
+			{mobs_mc.items.string, mobs_mc.items.stick, ''},
+			{mobs_mc.items.string, '', mobs_mc.items.stick},
+			{mobs_mc.items.string, mobs_mc.items.stick, ''},
+		}
+	})
 end
-
-minetest.register_tool("mobs_mc:bow_wood", {
-	description = "Bow",
-	inventory_image = "mobs_mc_bow.png",
-	on_use = function(itemstack, user, pointed_thing)
-		if throwing_shoot_arrow(itemstack, user, pointed_thing) then
-			if not minetest.settings:get_bool("creative_mode") then
-				itemstack:add_wear(65535/50)
-			end
-		end
-		return itemstack
-	end,
-})
-
-minetest.register_craft({
-	output = 'mobs_mc:bow_wood',
-	recipe = {
-		{mobs_mc.items.string, mobs_mc.items.stick, ''},
-		{mobs_mc.items.string, '', mobs_mc.items.stick},
-		{mobs_mc.items.string, mobs_mc.items.stick, ''},
-	}
-})
 
 -- egg throwing item
 -- egg entity
-mobs:register_arrow("mobs_mc:egg_entity", {
-	visual = "sprite",
-	visual_size = {x=.5, y=.5},
-	textures = {"mobs_chicken_egg.png"},
-	velocity = 6,
+if c("egg") then
+	mobs:register_arrow("mobs_mc:egg_entity", {
+		visual = "sprite",
+		visual_size = {x=.5, y=.5},
+		textures = {"mobs_chicken_egg.png"},
+		velocity = 6,
 
-	hit_player = function(self, player)
-		player:punch(minetest.get_player_by_name(self.playername) or self.object, 1.0, {
-			full_punch_interval = 1.0,
-			damage_groups = {},
-		}, nil)
-	end,
+		hit_player = function(self, player)
+			player:punch(minetest.get_player_by_name(self.playername) or self.object, 1.0, {
+				full_punch_interval = 1.0,
+				damage_groups = {},
+			}, nil)
+		end,
 
-	hit_mob = function(self, player)
-		player:punch(minetest.get_player_by_name(self.playername) or self.object, 1.0, {
-			full_punch_interval = 1.0,
-			damage_groups = {},
-		}, nil)
-	end,
+		hit_mob = function(self, player)
+			player:punch(minetest.get_player_by_name(self.playername) or self.object, 1.0, {
+				full_punch_interval = 1.0,
+				damage_groups = {},
+			}, nil)
+		end,
 
-	hit_node = function(self, pos, node)
+		hit_node = function(self, pos, node)
 
-		if math.random(1, 10) > 1 then
-			return
+			if math.random(1, 10) > 1 then
+				return
+			end
+
+			pos.y = pos.y + 1
+
+			local nod = minetest.get_node_or_nil(pos)
+
+			if not nod
+			or not minetest.registered_nodes[nod.name]
+			or minetest.registered_nodes[nod.name].walkable == true then
+				return
+			end
+
+			local mob = minetest.add_entity(pos, "mobs_mc:chicken")
+			local ent2 = mob:get_luaentity()
+
+			mob:set_properties({
+				visual_size = {
+					x = ent2.base_size.x / 2,
+					y = ent2.base_size.y / 2
+				},
+				collisionbox = {
+					ent2.base_colbox[1] / 2,
+					ent2.base_colbox[2] / 2,
+					ent2.base_colbox[3] / 2,
+					ent2.base_colbox[4] / 2,
+					ent2.base_colbox[5] / 2,
+					ent2.base_colbox[6] / 2
+				},
+			})
+
+			ent2.child = true
+			ent2.tamed = true
+			ent2.owner = self.playername
 		end
+	})
 
-		pos.y = pos.y + 1
+	local egg_GRAVITY = 9
+	local egg_VELOCITY = 19
 
-		local nod = minetest.get_node_or_nil(pos)
+	-- shoot egg
+	local mobs_shoot_egg = function (item, player, pointed_thing)
 
-		if not nod
-		or not minetest.registered_nodes[nod.name]
-		or minetest.registered_nodes[nod.name].walkable == true then
-			return
-		end
+		local playerpos = player:getpos()
 
-		local mob = minetest.add_entity(pos, "mobs_mc:chicken")
-		local ent2 = mob:get_luaentity()
-
-		mob:set_properties({
-			visual_size = {
-				x = ent2.base_size.x / 2,
-				y = ent2.base_size.y / 2
-			},
-			collisionbox = {
-				ent2.base_colbox[1] / 2,
-				ent2.base_colbox[2] / 2,
-				ent2.base_colbox[3] / 2,
-				ent2.base_colbox[4] / 2,
-				ent2.base_colbox[5] / 2,
-				ent2.base_colbox[6] / 2
-			},
+		minetest.sound_play("default_place_node_hard", {
+			pos = playerpos,
+			gain = 1.0,
+			max_hear_distance = 5,
 		})
 
-		ent2.child = true
-		ent2.tamed = true
-		ent2.owner = self.playername
+		local obj = minetest.add_entity({
+			x = playerpos.x,
+			y = playerpos.y +1.5,
+			z = playerpos.z
+		}, "mobs_mc:egg_entity")
+
+		local ent = obj:get_luaentity()
+		local dir = player:get_look_dir()
+
+		ent.velocity = egg_VELOCITY -- needed for api internal timing
+		ent.switch = 1 -- needed so that egg doesn't despawn straight away
+
+		obj:setvelocity({
+			x = dir.x * egg_VELOCITY,
+			y = dir.y * egg_VELOCITY,
+			z = dir.z * egg_VELOCITY
+		})
+
+		obj:setacceleration({
+			x = dir.x * -3,
+			y = -egg_GRAVITY,
+			z = dir.z * -3
+		})
+
+		-- pass player name to egg for chick ownership
+		local ent2 = obj:get_luaentity()
+		ent2.playername = player:get_player_name()
+
+		item:take_item()
+
+		return item
 	end
-})
 
-local egg_GRAVITY = 9
-local egg_VELOCITY = 19
-
--- shoot egg
-local mobs_shoot_egg = function (item, player, pointed_thing)
-
-	local playerpos = player:getpos()
-
-	minetest.sound_play("default_place_node_hard", {
-		pos = playerpos,
-		gain = 1.0,
-		max_hear_distance = 5,
+	minetest.register_craftitem("mobs_mc:egg", {
+		description = "Egg",
+		inventory_image = "mobs_chicken_egg.png",
+		on_use = mobs_shoot_egg,
 	})
-
-	local obj = minetest.add_entity({
-		x = playerpos.x,
-		y = playerpos.y +1.5,
-		z = playerpos.z
-	}, "mobs_mc:egg_entity")
-
-	local ent = obj:get_luaentity()
-	local dir = player:get_look_dir()
-
-	ent.velocity = egg_VELOCITY -- needed for api internal timing
-	ent.switch = 1 -- needed so that egg doesn't despawn straight away
-
-	obj:setvelocity({
-		x = dir.x * egg_VELOCITY,
-		y = dir.y * egg_VELOCITY,
-		z = dir.z * egg_VELOCITY
-	})
-
-	obj:setacceleration({
-		x = dir.x * -3,
-		y = -egg_GRAVITY,
-		z = dir.z * -3
-	})
-
-	-- pass player name to egg for chick ownership
-	local ent2 = obj:get_luaentity()
-	ent2.playername = player:get_player_name()
-
-	item:take_item()
-
-	return item
 end
-
--- chicken
-minetest.register_craftitem("mobs_mc:egg", {
-	description = "Egg",
-	inventory_image = "mobs_chicken_egg.png",
-	on_use = mobs_shoot_egg,
-})
 
 --end maikerumine code
 
