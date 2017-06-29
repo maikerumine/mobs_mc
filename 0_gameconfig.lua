f0630b771b84c0ee788a9e74d3596b041d77ea62
maikerumine <michaellorenger@yahoo.com>
2017-06-28 21:18:41 -0400

    Fix zombie pic spawning

--- 0_gameconfig.lua
+++ 0_gameconfig.lua
@@ -225,10 +225,10 @@ mobs_mc.spawn = {
 
 	-- These probably don't need overrides
 	mushroom_island = { mobs_mc.items.mycelium, "mcl_core:mycelium" },
-	nether_fortress = { mobs_mc.items.nether_brick_block, "mcl_nether:nether_brick" },
-	nether = { mobs_mc.items.netherrack, "mcl_nether:netherrack" },
+	nether_fortress = { mobs_mc.items.nether_brick_block, "mcl_nether:nether_brick", "mcl_portals:nether_air"  },
+	nether = { mobs_mc.items.netherrack, "mcl_nether:netherrack", "mcl_portals:nether_air" },
 	nether_portal = { mobs_mc.items.nether_portal, "mcl_portals:portal" },
-	water = { mobs_mc.items.water_source, "mcl_core:water_source" },
+	water = { mobs_mc.items.water_source, "mcl_core:water_source", "default:water_source" },
 }
 
 -- Item name overrides from mobs_mc_gameconfig (if present)
--- zombiepig.lua
+++ zombiepig.lua
@@ -96,7 +96,7 @@ mobs:register_spawn("mobs_mc:baby_pigman", mobs_mc.spawn.nether, minetest.LIGHT_
 --mobs:register_spawn("mobs_mc:pigman", {"nether:rack"},  17, -1, 5000, 3, -2000)
 mobs:register_spawn("mobs_mc:pigman", mobs_mc.spawn.nether, minetest.LIGHT_MAX+1, 0, 6000, 3, -2000)
 mobs:register_spawn("mobs_mc:pigman", mobs_mc.spawn.nether_portal, minetest.LIGHT_MAX+1, 0, 500, 4, 31000)
-mobs:spawn_specific("mobs_mc:pigman", mobs_mc.spawn.nether_portal, {"air"},0, minetest.LIGHT_MAX+1, 20, 9000, 2, -31000, 31000)
+mobs:spawn_specific("mobs_mc:pigman", mobs_mc.spawn.nether_portal, {"air", "mcl_portals:nether_air"},0, minetest.LIGHT_MAX+1, 7, 9000, 2, -31000, 31000)
 
 -- compatibility
 mobs:alias_mob("mobs:pigman", "mobs_mc:pigman")
