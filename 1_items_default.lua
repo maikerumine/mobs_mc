ba47f3b67de4ba84d7a69dcc80e20f88d3ad162b
Wuzzy <almikes@aol.com>
2018-01-25 19:54:53 +0100

    Show big icon when totem of undying activates

--- 1_items_default.lua
+++ 1_items_default.lua
@@ -519,6 +519,8 @@ end
 
 -- Evoker
 if c("totem") then
+	local hud_totem = {}
+
 	-- Totem of Undying
 	minetest.register_craftitem("mobs_mc:totem", {
 		description = S("Totem of Undying"),
@@ -528,6 +530,10 @@ if c("totem") then
 		wield_image = "mcl_totems_totem.png",
 		stack_max = 1,
 	})
+
+	minetest.register_on_leaveplayer(function(player)
+		hud_totem[player:get_player_name()] = nil
+	end)
 
 	-- Save the player from death when holding totem of undying in hand
 	minetest.register_on_player_hpchange(function(player, hp_change)
@@ -551,7 +557,29 @@ if c("totem") then
 					wield:take_item()
 					player:set_wielded_item(wield)
 				end
+				-- Effects
 				minetest.sound_play({name = "mcl_totems_totem", gain=1}, {pos=ppos, max_hear_distance=16})
+
+				-- Big totem overlay
+				if not hud_totem[player:get_player_name()] then
+					hud_totem[player:get_player_name()] = player:hud_add({
+						hud_elem_type = "image",
+						text = "mcl_totems_totem.png",
+						position = { x=0.5, y=1 },
+						scale = { x=17, y=17 },
+						offset = { x=0, y=-178 },
+					})
+					minetest.after(3, function(player)
+						if player and player:is_player() then
+							local name = player:get_player_name()
+							if hud_totem[name] then
+								player:hud_remove(hud_totem[name])
+								hud_totem[name] = nil
+							end
+						end
+					end, player)
+				end
+
 				-- Set HP to exactly 1
 				return -hp + 1
 			end
